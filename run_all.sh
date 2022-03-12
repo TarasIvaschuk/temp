802640b963cab1f3c079fd4a099c8c846489cfe2
Taras Ivashchuk <tarasivashchuk@gmail.com>
2022-03-13 01:10:43 +0200

    added sources

---
+++ run_all.sh
@@ -0,0 +1,8 @@
+IFS=$'\n' read -d "" -r -a lines < file.txt
+
+for i in "${lines[@]}"
+do
+    URL=$i
+    echo "docker run -d --rm alpine/bombardier -c 1000 -d 600000h -l $URL"
+    sudo docker run -d --rm alpine/bombardier -c 1000 -d 600000h -l $URL
+done
